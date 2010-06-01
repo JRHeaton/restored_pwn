fb9804473badbd417c234f260ab1ac9346846d78
John Heaton <jheaton.dev@gmail.com>
2010-05-31 22:04:59 -0400

    registry and nor setup

--- log.h
+++
@@ -1,8 +0,0 @@
-#ifndef RESTORED_PWN_LOG_H
-#define RESTORED_PWN_LOG_H
-
-#define log_function() restored_log("entering %s\n", __FUNC__);
-
-void restored_log(const char *fmt, ...);
-
-#endif /* RESTORED_PWN_LOG_H */
